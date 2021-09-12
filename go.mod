ae9df58ddb147d2bd55fa1bd896e1d815f3a5598
Suzukihonoka <sakurafirex@gmail.com>
2021-09-13 00:50:09 +0800

    fix some logic; update dependencies;

--- go.mod
+++ go.mod
@@ -1,17 +1,28 @@
 module ws_client
 
-go 1.16
+go 1.17
 
 require (
-	fyne.io/fyne/v2 v2.0.3
-	github.com/fyne-io/fyne-cross v1.1.0 // indirect
-	github.com/go-gl/gl v0.0.0-20210501111010-69f74958bac0 // indirect
+	fyne.io/fyne/v2 v2.0.4
+	github.com/go-gl/gl v0.0.0-20210905235341-f7a045908259 // indirect
 	github.com/gorilla/websocket v1.4.2
 	github.com/srwiley/oksvg v0.0.0-20210519022825-9fc0c575d5fe // indirect
 	github.com/srwiley/rasterx v0.0.0-20210519020934-456a8d69b780 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
-	golang.org/x/image v0.0.0-20210504121937-7319ad40d33e // indirect
-	golang.org/x/net v0.0.0-20210525063256-abc453219eb5 // indirect
-	golang.org/x/sys v0.0.0-20210603081109-ebe580a85c40 // indirect
+	golang.org/x/image v0.0.0-20210628002857-a66eb6448b8d // indirect
+	golang.org/x/net v0.0.0-20210908191846-a5e095526f91 // indirect
+	golang.org/x/sys v0.0.0-20210910150752-751e447fb3d0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fredbi/uri v0.0.0-20181227131451-3dcfdacbaaf3 // indirect
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/fyne-io/mobile v0.1.3-0.20210412090810-650a3139866a // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20210727001814-0db043d8d5be // indirect
+	github.com/godbus/dbus/v5 v5.0.5 // indirect
+	github.com/goki/freetype v0.0.0-20181231101311-fa8a33aabaff // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
--- main.go
+++ main.go
@@ -16,21 +16,18 @@ import (
 	"os"
 	"os/signal"
 	"strings"
-	"sync"
 	"time"
 )
 
 var (
 	myWindow fyne.Window
 	//
-	lock *safeLock
-	//lock   = false
 	failed bool
 	//
-	conn = false
+	conn bool
 	//
 	msgRec     *widget.Entry
-	inputAddr  *widget.Entry
+	inputAddr  *KeyDownEntryEx
 	connectBtn *widget.Button
 	//
 	para = &websocket.Dialer{
@@ -44,35 +41,30 @@ var (
 	//
 )
 
-type safeLock struct {
-	mu   sync.Mutex
-	lock bool
-}
-
-type multiLineEntryEx struct {
+type KeyDownEntryEx struct {
 	widget.Entry
 	action func()
 }
 
-func (e *multiLineEntryEx) KeyUp(key *fyne.KeyEvent) {
+func (e *KeyDownEntryEx) KeyUp(key *fyne.KeyEvent) {
 	if e.Disabled() {
 		return
 	}
 	if e.action != nil {
 		if key.Name == fyne.KeyEnter || key.Name == fyne.KeyReturn {
 			e.action()
+			fmt.Println("key down")
 		}
 	}
 }
 
-func NewMultiLineEntryEx() *multiLineEntryEx {
-	entry := &multiLineEntryEx{Entry: widget.Entry{MultiLine: true, Wrapping: fyne.TextTruncate}}
+func NewKeyDownEntryEx(multi bool) *KeyDownEntryEx {
+	entry := &KeyDownEntryEx{Entry: widget.Entry{MultiLine: multi, Wrapping: fyne.TextTruncate}}
 	entry.ExtendBaseWidget(entry)
 	return entry
 }
 
 func main() {
-	lock = &safeLock{}
 	signal.Notify(interrupt, os.Interrupt)
 	//
 	myApp := app.New()
@@ -83,15 +75,14 @@ func main() {
 		Height: 300,
 	})
 	msgRec = widget.NewMultiLineEntry()
-	inputAddr = widget.NewEntry()
-	connectBtn = widget.NewButton("Connect", func() {
-		go connect()
-	})
-	inputAddr.SetPlaceHolder("Enter server address...")
 	msgRec.SetPlaceHolder("Msg received")
-	msgToSend := NewMultiLineEntryEx()
+	inputAddr = NewKeyDownEntryEx(false)
+	inputAddr.SetPlaceHolder("Enter server address...")
+	inputAddr.action = proxy
+	connectBtn = widget.NewButton("Connect", proxy)
+	msgToSend := widget.NewMultiLineEntry()
 	msgToSend.SetPlaceHolder("Press enter to send message")
-	msgToSend.action = func() {
+	sendMsg := func() {
 		if !conn {
 			dialog.NewError(errors.New("you have to connect to the server first"), myWindow).Show()
 			return
@@ -100,51 +91,71 @@ func main() {
 		err := client.WriteMessage(websocket.TextMessage, []byte(msgToSend.Text))
 		if err != nil {
 			dialog.NewError(errors.New("send msg failed"), myWindow).Show()
-			//panic("send msg failed")
 		}
 		msgToSend.SetText("")
-		msgRec.SetText("")
+		//msgRec.SetText("")
 	}
+	send := widget.NewButton("Send", sendMsg)
 	//shortcutBtn := widget.NewButton("Shortcut", nil)
-	h2 := container.New(layout.NewVBoxLayout(), connectBtn)
-	card := widget.NewCard("Websocket Client", "A tool made by Starx", container.New(layout.NewVBoxLayout(), inputAddr, msgRec, msgToSend, h2))
+	//h2 := container.New(layout.NewVBoxLayout(), connectBtn)
+	card := widget.NewCard("Websocket Client", "A tool made by Starx", container.New(layout.NewVBoxLayout(), inputAddr, connectBtn, msgRec, msgToSend, send))
 	myWindow.SetContent(card)
 	myWindow.ShowAndRun()
+	go func() {
+		for {
+			select {
+			case <-interrupt:
+				if conn {
+					disconnect()
+				}
+				fmt.Println("interrupt")
+				os.Exit(1)
+			}
+		}
+	}()
+}
+
+func proxy() {
+	go connect()
 }
 
 func connect() {
-	if lock.lock {
-		fmt.Println("wtf")
+	if connectBtn.Disabled() {
 		return
 	}
 	if !conn {
+		connectBtn.Disable()
+		msgRec.SetText("")
 		if len(inputAddr.Text) == 0 {
 			dialog.NewError(errors.New("why not to write something"), myWindow).Show()
 			return
 		}
-		inputAddr.SetText(strings.ReplaceAll(inputAddr.Text, " ", ""))
+		inputAddr.SetText(strings.TrimSpace(inputAddr.Text))
 		failed = false
 		done = make(chan struct{})
 		_, err := url.Parse(inputAddr.Text)
 		if err != nil {
 			dialog.NewError(errors.New("parse server address failed"), myWindow).Show()
 			return
-			//panic("parse server address failed")
 		}
-		lock.mu.Lock()
-		lock.lock = true
+		connectBtn.SetText("Connecting")
 		client, _, err = para.Dial(inputAddr.Text, nil)
-		lock.lock = false
-		lock.mu.Unlock()
 		if err != nil {
-			dialog.NewError(errors.New("dial to server failed"), myWindow).Show()
+			dialog.NewError(errors.New("dial to server failed:\n"+err.Error()), myWindow).Show()
+			connectBtn.Enable()
+			connectBtn.SetText("Connect")
 			return
-			//panic("dial to server failed")
 		}
 		fmt.Println("ws connect ok")
 		conn = true
 		connectBtn.SetText("DisConnect")
-		defer client.Close()
+		connectBtn.Enable()
+		defer func(client *websocket.Conn) {
+			err := client.Close()
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+		}(client)
 		go func() {
 			defer close(done)
 			for conn {
@@ -159,34 +170,31 @@ func connect() {
 					dialog.NewError(err, myWindow).Show()
 					return
 				}
-				fmt.Printf("recv: %s", message)
-				fmt.Println()
+				fmt.Printf("recv: %s\n", message)
 				msgRec.SetText(msgRec.Text + string(message) + "\n")
 			}
 		}()
-
 		for conn {
 			select {
 			case <-done:
-				fmt.Println("done")
-				conn = false
+				disconnect()
 				connectBtn.SetText("Connect")
-				return
-			case <-interrupt:
-				fmt.Println("interrupt")
-				conn = false
-				err := client.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-				if err != nil {
-					fmt.Println("write close:", err)
-				}
-				return
+				connectBtn.Enable()
+				fmt.Println("done")
 			}
 		}
-	} else {
+		return
+	}
+	if failed {
 		conn = false
-		if !failed {
-			done <- struct{}{}
-		}
-		msgRec.SetText("")
+		return
+	}
+	done <- struct{}{}
+}
+
+func disconnect() {
+	err := client.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+	if err != nil {
+		fmt.Println("write close:", err)
 	}
 }
